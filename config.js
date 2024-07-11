0f7a167fb12ca334cc05aaa2013c6c04c996a1a4
Yasir Abbas <156669119+yasiraly07@users.noreply.github.com>
2024-07-11 11:57:03 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,9 +7,9 @@ global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
 global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "Yasir";
+global.owner = ['923555036392'];
+global.premium = ['923555036392'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
 global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
 global.sessionName = 'session';
